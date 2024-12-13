31f73e5519915b3fbae26cdcb42d047cfff191a0
Stéphane Caron <stephane.caron@inria.fr>
2024-12-13 10:29:20 +0100

    Add project description and version

--- pink_bench/__init__.py
+++ pink_bench/__init__.py
@@ -4,9 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Inria
 
+"""Library of robot motions with inverse kinematics and predictive control."""
+
 from .library import scenarios
 from .scenario import Scenario
 from .scene import Scene
+
+__version__ = "0.1.0"
 
 __all__ = [
     "Scenario",
